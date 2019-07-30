62efa2ee2f3bce91b8419aecd8e73766f07225db
兰林 <lanlin1987@gmail.com>
2019-07-30 17:30:05 +0800

    #

---
+++ src/Exception/GodConfigException.php
@@ -0,0 +1,14 @@
+<?php namespace God\Exception;
+
+/**
+ * ----------------------------------------------------------------------------------
+ * GodConfigException
+ * ----------------------------------------------------------------------------------
+ *
+ * @author lanlin
+ * @change 2019/07/30
+ */
+class GodConfigException extends \Exception
+{
+    // This is God Permission Exception
+}
